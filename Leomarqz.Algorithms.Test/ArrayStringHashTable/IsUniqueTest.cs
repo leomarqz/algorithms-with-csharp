cf9d06fe44a9038bcf495aff859c6eb25b1ad489
Elmer Marquez <leomarqz.main@gmail.com>
2025-11-11 20:56:28 -0600

    Add project files.

---
+++ Leomarqz.Algorithms.Test/ArrayStringHashTable/IsUniqueTest.cs
@@ -0,0 +1,50 @@
+﻿using Leomarqz.Algorithms.ArrayStringHashTable;
+
+namespace Leomarqz.Algorithms.Test.ArrayStringHashTable
+{
+    [TestClass]
+    public sealed class IsUniqueTest
+    {
+        [TestMethod]
+        public void IsUnique_ShouldReturnTrue_ForUniqueCharacters()
+        {
+            // Arrange
+            string input = "abcdefg"; //true
+            // Act
+            bool result = Unique.IsUnique(str: input);
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsUnique_ShouldReturnFalse_ForNonUniqueCharacters()
+        {
+            // Arrange
+            string input = "hello"; //false
+            // Act
+            bool result = Unique.IsUnique(str: input);
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        //fail
+        [TestMethod]
+        public void IsUnique_ShouldThrowArgumentException_ForEmptyString()
+        {
+            bool thrown = false;
+            // Arrange
+            string input = ""; // Exception
+            // Act & Assert
+            try
+            {
+                Unique.IsUnique(str: input);
+            }
+            catch (ArgumentException)
+            {
+                thrown = true;
+            }
+            Assert.IsTrue(thrown);
+        }
+
+    }
+}
---
+++ Leomarqz.Algorithms/ArrayStringHashTable/Unique.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Leomarqz.Algorithms.ArrayStringHashTable
+{
+    public class Unique
+    {
+        /// <summary>
+        /// Numero de caracteres ASCII estándar.
+        /// </summary>
+        private static readonly int NUMBER_OF_CHARS = 128;
+
+        /// <summary>
+        /// Determinar si todos los caracteres en una cadena son únicos.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>
+        /// True si todos los caracteres en la cadena son únicos, false en caso contrario.
+        /// </returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static bool IsUnique(string str)
+        {
+            HashSet<char> charSet = new HashSet<char>();
+
+            if (string.IsNullOrEmpty(str))
+                throw new ArgumentException("Input string cannot be null or empty.");
+
+            if (str.Length > NUMBER_OF_CHARS) return false;
+
+            foreach(char c in str)
+            {
+                if(charSet.Contains(c))
+                {
+                    return false;
+                }
+                charSet.Add(c);
+            }
+
+            return true;
+        }
+    }
+}
